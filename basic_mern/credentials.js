09c637bfda10f660e56af7d1013084dd64de732b
Michael Shen <michael.shen.100@gmail.com>
2019-03-05 18:40:49 -0600

    Changed framework
    using simplified framework now

---
+++ basic_mern/credentials.js
@@ -0,0 +1,3 @@
+const dbRoute = "mongodb+srv://mshen:HS9vLics85zjwVb@cluster0-zbis6.mongodb.net/test?retryWrites=true"
+
+module.exports = {dbRoute: dbRoute}
---
+++ basic_mern/server.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const credentials = require('./credentials')
+const Data = require('./data')
+
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+mongoose.connect(credentials.dbRoute, {useNewUrlParser: true})
+let db = mongoose.connection
+
+db.once("open", () => console.log("Connected to the database."))
+db.on("error", console.error.bind(console, "MongoDB connection error:"))
+
+const port = 5000
+
+app.post("/write_data", (req, res) => {
+  let data = new Data()
+  const {message} = req.body
+
+  if (!message) return res.json({success: false, error: "Invalid input."})
+
+  data.message = message
+  data.save(err => {
+    if (err) return res.json({success: false, error: err})
+    return res.json({success: true})
+  })
+})
+
+app.get("/get_data", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({success: false, error: err})
+    return res.json({success: true, data: data})
+  })
+})
+
+app.delete("/delete_data", (req, res) => {
+  const {id} = req.body
+  Data.findByIdAndDelete(id, (err) => {
+    if (err) return res.send(err)
+    return res.json({success: true})
+  })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
