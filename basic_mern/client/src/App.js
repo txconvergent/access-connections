4e92451bb2b45f03ec4385ec22735b7149bbf507
Michael Shen <michael.shen.100@gmail.com>
2019-03-26 18:33:33 -0500

    added bootstrap navbar

--- basic_mern/client/src/App.js
+++ basic_mern/client/src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import logo from './logo.svg';
