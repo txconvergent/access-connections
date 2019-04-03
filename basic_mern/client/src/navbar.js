581fcdc2f6bf41c1770f305d05bf3212ad8b5a46
alanzeng17 <alanzeng17@gmail.com>
2019-04-03 17:53:47 -0500

    asd

--- basic_mern/client/src/navbar.js
+++ basic_mern/client/src/navbar.js
@@ -11,13 +11,13 @@ class Navbar extends React.Component{
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
-                    <li class="nav-item">
+                    <li class="nav-item active">
                     <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact</a>
                     </li>
-                    <li class="nav-item active">
+                    <li class="nav-item">
                     <a class="nav-link" href="/login">Login <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
