9ee63ea99b62bd9d890f1f86ddf02876ea24ada6
alanzeng17 <alanzeng17@gmail.com>
2019-04-03 17:53:47 -0500

    asd

--- basic_mern/client/src/home.js
+++ basic_mern/client/src/home.js
@@ -1,62 +1,33 @@
 import React from 'react';
 import axios from "axios";
+import {Jumbotron, Container, Button, Row, Col} from 'react-bootstrap'
 
 class Home extends React.Component {
-  getDataFromDB = () => {
-    fetch('/get_data')
-      .then((data) => data.json())
-      .then((res) => this.setState({data: res.data}))
-  }
-
-  writeDataToDB = (message) => {
-    axios.post('/write_data', {message: message})
-      .then(() => this.getDataFromDB())
-  }
-
-  deleteDataFromDB = (id) => {
-    axios.delete('/delete_data', {data: {id: id}})
-      .then(() => this.getDataFromDB())
-  }
-  
-  state = {
-    data: [],
-    message: null
-  }
-
-  componentDidMount() {
-    this.getDataFromDB()
-  }
 
   render() {
     return(
       <div>
-        <div>
-          <ul>
-            {this.state.data.length <= 0
-              ? "No entries in database yet."
-              : this.state.data.map(dat => (
-                <li style={{padding: "10px"}} key={dat._id}>
-                  <span style={{color: "gray"}}> id: </span> {dat._id} <br/>
-                  <span style={{color: "gray"}}> data: </span> {dat.message}
-                  <span style={{color: "gray"}}> user: </span> {dat.user}
-                  <span style={{color: "gray"}}> pass: </span> {dat.pass}
-                </li>
-              ))}
-          </ul>
-        </div>
-        <div style={{padding: "10px"}}>
-          <input type="text" onChange={(e) => this.setState({ message: e.target.value })}
-            placeholder="add something in the database" style={{width: "200px"}}/>
-          <button onClick={() => this.writeDataToDB(this.state.message)}>add</button>
-        </div>
-        <div>
-          <input type="text" onChange={(e) => this.setState({ id: e.target.value })}
-            placeholder="delete something in the database" style={{width: "200px"}}/>
-          <button onClick={() => this.deleteDataFromDB(this.state.id)}>delete</button>
-        </div>
-        <div style={{padding: "10px"}}>
-          <button onClick={() => this.getDataFromDB()}>load</button>
-        </div>
+        <Jumbotron style={{backgroundColor: '#90caf9'}}>
+          <Container>
+            <div style={{paddingTop: '150px', paddingBottom: '100px'}}>
+              <h1>ConnectMeNow</h1>
+              <p>
+                We connect people to other people. 
+              </p>
+              <Button large href="/about">About Us</Button>
+              <Container style={{paddingTop: '25px'}}>
+                <Row>
+                   <Button>Log In</Button>
+                    <Button style={{marginLeft: "50px"}}>Sign Up</Button>
+                </Row>
+              </Container>
+            </div>
+          </Container>
+        </Jumbotron>
+        <Container style={{textAlign: "center"}}>
+          <h1>How it works</h1>
+          <p>Lorem Ipsum Dolor latin </p>
+        </Container>
       </div>
     )
   }
